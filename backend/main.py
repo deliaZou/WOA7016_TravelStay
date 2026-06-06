976566e6f22c5edf1261a13c39b5f21412cdafd9
ZOU TING <delia0427@gmail.com>
2026-06-06 14:11:23 +0800

    monitor setting2

--- backend/main.py
+++ backend/main.py
@@ -147,3 +147,6 @@ if __name__ == "__main__":
     init_db()
     # 2. Fire up the Uvicorn local web server
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+
+Instrumentator().instrument(app).expose(app)
